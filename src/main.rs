cd8d98463e4edc815f2d2a37ac79404de1147444
Shivam Balikondwar <shivambalikondwar@icloud.com>
2020-05-08 19:12:06 +0530

    WIP: Performing work to multi-thread the server.
      Follow up.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,45 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs;
+use std::thread;
+use std::time::Duration;
+
+use web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connected");
-
-        handle_con(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
-fn handle_con(mut stream: TcpStream){
+fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    //println!("request {}", String::from_utf8_lossy(&buffer[..]));
-
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    if buffer.starts_with(get){
-
-    let cont = fs::read_to_string("sample_html.html").unwrap();
-
-    let resp = format!("HTTP/1.1 200 OK\r\n\r\n{}",cont);
-
-    stream.write(resp.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "sample_html.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "sample_html.html")
     } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
 
-    }
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
